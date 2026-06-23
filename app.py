6496dfab7b77c5ccd5ff9fc7a82ca4a06ed82c8f
Shukdev Datta <99469701+shukdevtroy@users.noreply.github.com>
2026-06-23 20:32:35 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1106,4 +1106,4 @@ with gr.Blocks(css=custom_css, theme="ocean") as demo:
 
 # Run the app
 if __name__ == "__main__":
-    demo.launch(share=True)
+    demo.launch(server_name="0.0.0.0",server_port=int(os.environ.get("PORT", 7860)))
